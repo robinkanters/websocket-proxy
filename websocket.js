f6ea78db3f7fe6ca133e9ac360974c3cfb9f1324
Robin Kanters <robin@robinkanters.nl>
2015-12-04 21:39:32 +0100

    Added copyright notice

--- websocket.js
+++ websocket.js
@@ -1,22 +1,29 @@
+/**
+ * Copyright (C) 2015 Robin Kanters
+ *
+ * For more info, see:
+ * https://raw.githubusercontent.com/robinkanters/websocket-proxy/master/LICENSE
+ */
+
 var ws = require('nodejs-websocket');
 var http = require('http');
 
-if(process.argv.length == 4) {
-  ws.connect(process.argv[2]).on('text', function (data) {
-    var url = process.argv[3];
-    var dataString = encodeURIComponent(data);
-    var fullUrl = url + "?data=" + dataString;
+if (process.argv.length == 4) {
+    ws.connect(process.argv[2]).on('text', function (data) {
+        var url = process.argv[3];
+        var dataString = encodeURIComponent(data);
+        var fullUrl = url + "?data=" + dataString;
 
-    http.get(fullUrl, function (res) {
-      console.log("success " + res);
-    }).on('error', function (e) {
-      console.log("error " + e.message);
+        http.get(fullUrl, function (res) {
+            console.log("success " + res);
+        }).on('error', function (e) {
+            console.log("error " + e.message);
+        });
     });
-  });
 } else {
-  console.log('Correct usage:');
-  console.log(process.argv[0] + ' ' + process.argv[1] + ' <web socket endpoint> <target url>');
-  console.log('');
-  console.log('For example:');
-  console.log(process.argv[0] + ' ' + process.argv[1] + ' wss://socket.example.org http://localhost/callback');
+    console.log('Correct usage:');
+    console.log(process.argv[0] + ' ' + process.argv[1] + ' <web socket endpoint> <target url>');
+    console.log('');
+    console.log('For example:');
+    console.log(process.argv[0] + ' ' + process.argv[1] + ' wss://socket.example.org http://localhost/callback');
 }
