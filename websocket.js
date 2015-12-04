478091c7b5628f09b5dd31a365ade49d924f1c04
Robin Kanters <robin@robinkanters.nl>
2015-12-04 22:26:12 +0100

    Pulled usage message out into a function

--- websocket.js
+++ websocket.js
@@ -8,22 +8,29 @@
 var ws = require('nodejs-websocket');
 var http = require('http');
 
-if (process.argv.length == 4) {
-    ws.connect(process.argv[2]).on('text', function (data) {
-        var url = process.argv[3];
-        var dataString = encodeURIComponent(data);
-        var fullUrl = url + "?data=" + dataString;
+if (process.argv.length != 4) {
+    printHelp();
+    process.exit();
+}
 
-        http.get(fullUrl, function (res) {
-            console.log("success " + res);
-        }).on('error', function (e) {
-            console.log("error " + e.message);
-        });
+ws.connect(process.argv[2]).on('text', function (data) {
+    var url = process.argv[3];
+    var dataString = encodeURIComponent(data);
+    var fullUrl = url + "?data=" + dataString;
+
+    http.get(fullUrl, function (res) {
+        console.log("success " + res);
+    }).on('error', function (e) {
+        console.log("error " + e.message);
     });
-} else {
+});
+
+function printHelp() {
     console.log('Correct usage:');
     console.log(process.argv[0] + ' ' + process.argv[1] + ' <web socket endpoint> <target url>');
     console.log('');
     console.log('For example:');
     console.log(process.argv[0] + ' ' + process.argv[1] + ' wss://socket.example.org http://localhost/callback');
+
+    process.exit();
 }
